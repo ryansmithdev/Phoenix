b4f178670aff9370afe4df7f0e74ef9386244de4
Matthew Fain <matthewfain96@icloud.com>
2014-04-30 15:56:36 -0400

    Deletion
    Deleted everything in the file

--- test.php
+++ test.php
@@ -1,8 +1,1 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-    $.get("http://localhost2/index.php",function(data){
-      document.write(data);
-    });
-  });
-</script>
+anything
