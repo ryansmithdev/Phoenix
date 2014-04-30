74e7c4cf2c3115777a49a86850ccba3f317d60c6
Ryan Smith <ryanesmith@me.com>
2014-04-29 23:45:52 -0400

    Added permission group management functions: create_permission_group, delete_permission_group, add_inheritance_to_permission_group.

--- phoenix/includes/class-Commands.php
+++ phoenix/includes/class-Commands.php
@@ -31,7 +31,36 @@ class Commands {
 		
 	}
 	
-	public function add_user_to_group( $uid, $group ) {
+	public function create_permission_group( $name, $inherit = null ) {
+	
+		Console::tell("Creating perm group $name with inheritance $inherit");
+		
+		$permissions = new Permissions();
+		$return = $permissions->createGroup( $name, null, $inherit );
+		
+		return ($return) ? "Created permission group $name successfully." : "Failed to create inheritance group $group. Try using delete_permission_group <group_name> first.";
+		
+	}
+	
+	public function delete_permission_group( $name ) {
+		
+		$permissions = new Permissions();
+		$return = $permissions->deleteGroup( $name );
+		
+		return ($return) ? "Deleted permission group $name successfully." : "Failed to delete inheritance group $group.";
+		
+	}
+	
+	public function add_inheritance_to_permission_group( $group, $inherit ) {
+		
+		$permissions = new Permissions();
+		$return = $permissions->addInheritaceToGroup( $group, $inherit );
+		
+		return ($return) ? "Inheritance for group $group changed to $inherit successfully." : "Inheritance change for group $group failed.";
+		
+	}
+	
+	public function add_user_to_permission_group( $uid, $group ) {
 		
 		/*
 			
